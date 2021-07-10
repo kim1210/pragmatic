d091db110d72a213c9564f9f6c4eb3f9cada2216
kim1210 <69778811+kim1210@users.noreply.github.com>
2021-07-10 19:36:55 +0900

    django cousre 22 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,19 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+
+    # login과 logout은 작성해야 할 세부사항이 많지 않으므로 views.py에서 별도로 지정하지 않고,
+    # urls.py에서 바로 가져와도 괜찮다.
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    # 함수형으로 가져올 때와 달리, class형을 가져올 때는, .as_view()를 붙여와야 한다.
+    # 그래야 함수형처럼 기능을 한다.
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
-
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
+from django.views.generic import CreateView
+
 from accountapp.models import HelloWorld
 
 def hello_world(request):
@@ -21,3 +24,14 @@ def hello_world(request):
     else :
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    # 함수형에서는 reverse를 사용하고 class형에서는 reverse_lazy를 사용한다.
+
+    template_name = 'accountapp/create.html'
+    # 어떤 템플릿을 보여줄지 설정~
+
