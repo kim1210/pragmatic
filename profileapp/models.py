3d2add767d30b54f12c79e2df6f0f5616c50d90c
kim1210 <69778811+kim1210@users.noreply.github.com>
2021-07-16 21:04:06 +0900

    django course 30 commit

---
+++ profileapp/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    image = models.ImageField(upload_to='profile/', null = True)
+    # image가 저장되는 root를 upload_to로 지정해준다.
+    # media밑에 profile이라는 폴더 아래의 경로로 저장될 것이다.
+    nickname = models.CharField(max_length=20, unique=True, null=True)
+    message = models.CharField(max_length=100, null=True)
