9764c29520fdf4f01491abd0be30b8ebcac34899
kim1210 <69778811+kim1210@users.noreply.github.com>
2021-08-20 15:29:37 +0900

    django course 35 commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,16 @@
+# 35강
+
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model) :
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    # on_delete=models.SET_NULL 설정은, 회원 탈퇴시 게시글의 writer가 NULL값으로 되도록 하는 것
+
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    created_at = models.DateField(auto_now_add=True, null=True)
--- profileapp/models.py
+++ profileapp/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-
     image = models.ImageField(upload_to='profile/', null = True)
     # image가 저장되는 root를 upload_to로 지정해준다.
     # media밑에 profile이라는 폴더 아래의 경로로 저장될 것이다.
